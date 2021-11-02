11582454574ce5294b3af5cc441899a1f0dcbab6
DesPetrikov <desik1603@gmail.com>
2021-11-03 00:00:43 +0300

    feat: add solution to task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const text = path.join(__dirname, 'text.txt')
+const stream = new fs.ReadStream(text, {encoding: 'utf-8'});
+
+
+stream.on('readable', () => {
+	let data;
+	while((data = stream.read()) !== null){
+		console.log(data);
+	}
+});
+
+stream.on('error', (err) => {
+	if(err.code == 'ENOENT'){
+		console.log(`В директории ${path.dirname(__filename)} не найден файл ${path.basename(text)}`);
+	} 
+	else{
+		console.error(err);
+	}
+})
